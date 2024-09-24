fb5b80edb8ad9d156b32c3d2102e706a59ca8f5b
rafashayo <49433616@est.ort.edu.ar>
2024-09-24 15:41:29 -0300

    Rafa: cambios en login, register y favorito

--- src/lib/loginSoquetic.js
+++ src/lib/loginSoquetic.js
@@ -1,10 +1,31 @@
 import { onEvent, sendEvent, startServer } from "soquetic";
-import { writeFile, readFileSync, readFile, fstat, fdatasync, writeFileSync } from "fs";
+import { writeFile, readFileSync, readFile, fstat, fdatasync, writeFileSync, ftruncateSync } from "fs";
 import { join } from "path";
-import { randomUUID } from "crypto"
+import { randomUUID } from "crypto";
 
-const pathJSON = join("./data/users.json")
+const pathJSON = join("./data/users.json");
+
+function error() {
+    //TODO:función que haga que diga "nombre de usuario o contraseña incorrectos"
+}
+
+function confirmLogin() {
+    //TODO:función para iniciar sesión
+}
 
 function login() {
-    readFileSync(pathJSON)
+    readFile(pathJSON, "utf-8", (err, info) => {
+        onEvent("iniciarSesion", (data) => {
+            if(data.username == info.username || data.email == info.data) {
+                
+            } if (data.password == info.password) {
+                confirmLogin();
+            } else {
+                error();
+            }
+        })
+    })
+
 }
+
+login();
---
+++ src/lib/marcarFavorito.js
@@ -0,0 +1,11 @@
+import { onEvent, sendEvent, startServer } from "soquetic";
+import { writeFile, readFileSync, readFile, fstat, fdatasync, writeFileSync, ftruncateSync } from "fs";
+import { join } from "path";
+import { randomUUID } from "crypto";
+
+const pathJSON = join("./data/users.json");
+
+function favorito() {
+    //TODO:que cuando un arrchivo se marca favorito se añada en el usuario su favorito y que haga fetch de cualaes son sus lecciones favoritas para marcarlas
+    writeFileSync(pathJSON)
+}
