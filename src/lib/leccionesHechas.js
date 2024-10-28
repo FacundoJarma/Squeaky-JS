a956fdb16f48a879575c2de58eaffb4a6d864225
facujarma <facundojarma@gmail.com>
2024-10-28 10:17:34 -0300

    Facu: Agregar leccion hecha

--- src/lib/leccionesHechas.js
+++ src/lib/leccionesHechas.js
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 import { join } from 'path';
 
-const pathJSON = join("data/users.json");
+const pathJSON = join("./src/lib/data/users.json");
 
 export async function leccionesHecha(data) {
     try {
