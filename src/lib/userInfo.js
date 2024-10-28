07a199a2e43b7d143c537fe2e13b6cb572302078
rafashayo <you@example.com>
2024-10-28 10:36:35 -0300

    Rafa: más de userinfo

---
+++ src/lib/userInfo.js
@@ -0,0 +1,37 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+const pathJSON = join("data/users.json");
+
+export async function userDisplay(data) {
+    try {
+        const leido = await fs.readFile(pathJSON, 'utf-8');
+        let jsonData = JSON.parse(leido);
+
+        const usuario = jsonData.find(usuario => usuario.username === data.username); 
+        if (!usuario) {
+            console.error('Usuario no encontrado en userinfo.js');
+            return;
+        } else {
+            console.log(usuario.racha)
+            let uData = {
+                racha: usuario.racha,
+                username: usuario.username,
+                email: usuario.email,
+                fechaDeNacimiento: usuario.fechaDeNacimiento,
+                password: usuario.password
+            }
+            return usuario.racha;
+            return usuario.username;
+            return usuario.email;
+            return usuario.fechaDeNacimiento;
+            return usuario.password;
+        }
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+userDisplay({
+    username: 'rafa'
+})
