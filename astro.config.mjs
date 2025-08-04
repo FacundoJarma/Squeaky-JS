2bba121d7f44d3b4f2f88e0d0b8098e191c821d7
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-04 08:51:16 -0300

    añadir adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless"; 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: vercel()
 });
